04daa9d1ac089d0f08d84803ece90cec8f9665df
PapaJoesSoup <joekorinek@cox.net>
2015-11-16 17:54:29 -0600

    Distribution reorg:  add Config Folder, move mm configs.

--- buildrelease.bat
+++ buildrelease.bat
@@ -5,15 +5,15 @@ mkdir GameData\ConnectedLivingSpace\assets
 mkdir GameData\ConnectedLivingSpace\Plugins
 
 copy assets\* GameData\ConnectedLivingSpace\assets
-copy configs\* GameData\ConnectedLivingSpace\Plugins
+copy configs\* GameData\ConnectedLivingSpace\Configs
 
 copy plugins\CLSInterfaces\bin\Release\CLSInterfaces.dll GameData\ConnectedLivingSpace\Plugins
 copy plugins\ConnectedLivingSpace\bin\Release\ConnectedLivingSpace.dll GameData\ConnectedLivingSpace\Plugins
 
 delete release\CLSv%1.zip
 
-"c:\program files (x86)\7zip\7za.exe" a -r release\CLSv%1.zip GameData\*
-"c:\program files (x86)\7zip\7za.exe" a -r release\CLSv%1.zip README.txt
+"c:\7zip\7za.exe" a -r release\CLSv%1.zip GameData\*
+"c:\7zip\7za.exe" a -r release\CLSv%1.zip README.txt
 
 rmdir /S /Q GameData
 
@@ -22,8 +22,8 @@ copy plugins\CLSInterfaces\bin\Release\CLSInterfaces.dll dev
 
 copy plugins\CLSInterfaces\CLSClient.cs dev
 
-"c:\program files (x86)\7zip\7za.exe" a -r dev\CLSDevPackv%1.zip dev\*
-"c:\program files (x86)\7zip\7za.exe" a -r release\CLSv%1.zip dev\CLSDevPackv%1.zip
+"c:\7zip\7za.exe" a -r dev\CLSDevPackv%1.zip dev\*
+"c:\7zip\7za.exe" a -r release\CLSv%1.zip dev\CLSDevPackv%1.zip
 
 rmdir /S /Q dev
 
