5f967ebe32b140e013bb050479f0375b916f060c
PapaJoesSoup <joekorinek@cox.net>
2018-04-09 20:06:42 -0500

    1.2.6.1 final cleanup

--- interfacepostbuild.bat
+++
@@ -1,7 +0,0 @@
-copy /Y "$(TargetPath)" "C:\Games\KSP\Kerbal Space Program1.2.1\GameData\ConnectedLivingSpace\Plugins\"
-copy /Y "$(TargetPath)" "C:\Games\KSP\Kerbal Space Program1.2.1\GameData\ShipManifest\Plugins\"
-copy /Y "$(TargetPath)" "D:\KSP\Mods\PJS\Releases\ConnectedLivingSpace\Build\CLSDevPack\"
-copy /Y "$(TargetPath)" "D:\KSP\Mods\PJS\Git\Source\ShipManifest\ShipManifest\Distribution\GameData\ShipManifest\Plugins\"
-copy /Y "$(TargetPath)" "D:\KSP\Mods\PJS\Git\Source\ShipManifest\ShipManifest\bin\debug\"
-copy /Y "$(ProjectDir)CLSClient.cs" "D:\KSP\Mods\PJS\Releases\ConnectedLivingSpace\Build\CLSDevPack\"
-
