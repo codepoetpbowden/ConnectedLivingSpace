c5696b310e8540ea3ec89b7b4d66686d3df6b31a
Michael Werle <micha@michaelwerle.com>
2020-03-04 00:26:05 +0900

    Project reorganisation to clean up top-level git repo.

--- Source/ConnectedLivingSpace/Properties/AssemblyInfo.cs
+++ Source/ConnectedLivingSpace/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CLSInterfaces")]
-[assembly: AssemblyDescription("Interface definitions for Connected Living Space")]
+[assembly: AssemblyTitle("ConnectedLivingSpace")]
+[assembly: AssemblyDescription("KSP Plugin to detect internally connected living spaces")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CLSInterfaces")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("PWB")]
+[assembly: AssemblyProduct("ConnectedLivingSpace")]
+[assembly: AssemblyCopyright("Copyright ©  2014, 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f6c36fe0-15d3-494a-b21e-4d0c43e57ba0")]
+[assembly: Guid("39ece0ec-3186-45d4-9cfd-bac6548c3f72")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+//[assembly: AssemblyFileVersion("2.0.0.3")]
